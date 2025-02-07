20c937c2175dff7f59862cde7c0c4514e1013b41
DinIskakov <dinizkakov@gmail.com>
2025-02-07 16:53:24 +0000

    Fix paths

--- backend/google_authenticator.py
+++ backend/google_authenticator.py
@@ -11,7 +11,7 @@ SCOPES = ["https://www.googleapis.com/auth/calendar"]
 
 def initialize_service():
     creds = None
-    
+
     if os.path.exists("token.json"):
         creds = Credentials.from_authorized_user_file("token.json")
 
@@ -19,7 +19,7 @@ def initialize_service():
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
+            flow = InstalledAppFlow.from_client_secrets_file("../credentials.json", SCOPES)
             creds = flow.run_local_server(port=0)
 
         with open("token.json", "w") as token:
